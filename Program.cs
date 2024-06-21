3e51dcd9834e90f65c93b023eb5fd6c1e81f2f9c
Alfred Odey <alfredmodey@gmail.com>
2024-06-22 00:00:29 +0100

    Question 7

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAndManipulateArray
+{
+    internal class Program
+    {
+        /*
+        Question 7: Create and Manipulate an Array
+        Create an array of integers with the values
+        {10, 20, 30, 40, 50}.
+        Print the length of the array.
+        Change the third element to 35.
+        Print all elements in the array.
+        */
+        static void Main(string[] args)
+        {
+            int[] integers = { 10, 20, 30, 40, 50 };
+
+            Console.WriteLine($"Length of array: {integers.Length}\n");
+
+            integers[2] = 35;
+
+            Console.WriteLine($"Elements of the array: {String.Join(", ", integers)}\n");
+
+            /*
+            for (int x = 0; x < integers.Length; x++)
+            {
+                Console.Write($"{integers[x]} ");
+            }
+            */
+        }
+    }
+}
